df2e13cd18fa76606cad144598cf240d31bf04bb
up-chen <1213779256@qq.com>
2017-07-25 01:15:07 +0800

    set up the environment with webpack

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+var path = require('path')
+var webpack = require('webpack')
+
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var op = require('open-browser-webpack-plugin')
+
+module.exports = {
+	entry: path.resolve(__dirname, 'app/index.js'),
+	output:{
+		path: path.resolve(__dirname, 'app/index.js'),
+		filename: "dist/bundle.js"
+	},
+	resolve: {
+        extensions: ['.js','jsx']
+    },
+	module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                include: /app/,
+                use: ["babel-loader"]
+            },
+            {
+                test: /\.css$/,
+                use : [
+                    {
+                        loader : 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            importLoaders: 1
+                        }
+                    },
+                    {
+                        loader: 'postcss-loader',
+                        options : {
+                            plugins : function() {
+                                return [
+                                    require('autoprefixer')({
+                                        broswers : ['last 5 versions']
+                                    })
+                                ];
+                            }
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.less$/,
+                loader: 'style-loader!css-loader!less-loader'
+            },
+            {
+                test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+                loader: 'url-loader'
+            },
+            {
+                test: /\.(png|jpg|svg|gif|jpeg|bmp)$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 5000
+                }
+            }
+        ]
+    },
+    plugins: [
+    	//往前端塞一个全局变量 true or false
+        new webpack.DefinePlugin({
+            __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+        }),
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.LoaderOptionsPlugin(),
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: __dirname + '/app/index.html'
+        }),
+         // webpack完成后自动打开浏览器
+        new op({
+            url: 'http://localhost:3000'
+        })
+    ],
+    devtool: 'eval-source-map',
+    devServer: {
+      //此处是webpack-dev-server的配置
+        contentBase: './public',
+        //不跳转，在开发单页应用时非常有用，它依赖于HTML5 history API，如果设置为true，所有的跳转将指向index.html
+        historyApiFallback: true,
+        inline: true,
+        hot: true,
+        port:3000 
+    }
+
+
+}
---
+++ webpack.product.config.js
@@ -0,0 +1,141 @@
+var pkg = require('./package.json')
+var path = require('path')
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: path.resolve(__dirname, 'app/index.js'),
+    // 将第三方依赖（node_modules中的）单独打包
+    vendor: Object.keys(pkg.dependencies)
+  },
+  output: {
+    path: __dirname + "/build",
+    filename: "/js/[name].[chunkhash:8].js"
+  },
+
+  resolve:{
+      extensions: ['.js','.jsx']
+  },
+
+  module: {
+    rules: [
+        { 
+            test: /\.(js|jsx)$/,
+            exclude: /node_modules/,
+            loader: "babel-loader"
+        },
+        { 
+            test: /\.less$/, 
+            exclude: /node_modules/, 
+            loader: ExtractTextPlugin.extract({
+                fallback: 'style-loader', 
+                use: [
+                    {
+                        loader:'css-loader'
+                    }, 
+                    {
+                        loader: 'postcss-loader',
+                        options : {
+                            plugins : function() {
+                                return [
+                                    require('autoprefixer')({
+                                        broswers : ['last 5 versions']
+                                    })
+                                ];
+                            }
+                        }
+                    },
+                    {
+                        loader: 'less-loader'
+                    }
+                ]
+            })
+        },
+        { 
+            test: /\.css$/,
+            exclude: /node_modules/,
+            use : ExtractTextPlugin.extract({
+                fallback: 'style-loader',
+                use: [
+                    {
+                        loader:'css-loader'
+                    }, 
+                    {
+                        loader: 'postcss-loader',
+                        options : {
+                            plugins : function() {
+                                return [
+                                    require('autoprefixer')({
+                                        broswers : ['last 5 versions']
+                                    })
+                                ];
+                            }
+                        }
+                    }
+                ]
+            })
+        },
+        { 
+            test:/\.(png|gif|jpg|jpeg|bmp)$/i, 
+            loader:'url-loader',
+            options:{
+                limit: 5000,
+                name: 'img/[name].[chunkhash:8].[ext]'
+            } 
+        },
+
+        { 
+            test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, 
+            loader:'url-loader',
+            options: {
+                limit: 5000,
+                name: 'fonts/[name].[chunkhash:8].[ext]'
+            }
+        }
+    ]
+  },
+  devtool: 'source-map',
+  plugins: [
+    // webpack 内置的 banner-plugin
+    new webpack.BannerPlugin({
+        baner: "Copyright by wangfupeng1988@github.com."
+    }),
+
+    // html 模板插件
+    new HtmlWebpackPlugin({
+        template: __dirname + '/app/index.html'
+    }),
+
+    // 定义为生产环境，编译 React 时压缩到最小
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      }
+    }),
+    
+    new webpack.optimize.UglifyJsPlugin({
+        compress: {
+          //supresses warnings, usually from module minification
+          warnings: false
+        }
+    }),
+    
+    // 分离CSS和JS文件
+    new ExtractTextPlugin({
+        filename: '/css/[name].[chunkhash:8].css'
+    }), 
+    
+    // 提供公共代码
+    new webpack.optimize.CommonsChunkPlugin({
+      name: 'vendor',
+      filename: '/js/[name].[chunkhash:8].js'
+    }),
+
+    // 可在业务 js 代码中使用 __DEV__ 判断是否是dev模式（dev模式下可以提示错误、测试报告等, production模式不提示）
+    new webpack.DefinePlugin({
+      __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+    })
+  ]
+}
